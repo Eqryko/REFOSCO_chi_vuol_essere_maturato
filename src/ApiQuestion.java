7365ef11bc556adad2b0b1c2f86bdc9e902e0133
Harry <enricoorefosco@gmail.com>
2025-10-22 07:59:44 +0200

    Update

--- src/ApiQuestion.java
+++ src/ApiQuestion.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ApiQuestion {
     public String question;
     public String correct_answer;
@@ -5,4 +9,9 @@ public class ApiQuestion {
     public String difficulty;
     public String[] incorrect_answers;
 
+    /*public Map<Character, String> answerList = new LinkedHashMap<>();
+    public Map<Character, String> formulateAnswer(){
+        List<String> ansList = new ArrayList<>(List.of(correct_answer));
+        ansList.addAll(List)
+    }*/
 }
---
+++ src/PlayerStatistics.java
@@ -0,0 +1,10 @@
+public class PlayerStatistics {
+    private String name;
+    private int correctAnswers;
+    private boolean used5050;
+    private boolean usedAudience;
+
+    public PlayerStatistics(String name, int correctAnswers, boolean used5050, boolean usedAudience) {
+
+    }
+}
